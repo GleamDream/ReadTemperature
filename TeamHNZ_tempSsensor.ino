87437326af97f3b67666b2d815f818596f3a67b3
Piromu <ic141202@edu.okinawa-ct.ac.jp>
2019-08-01 11:11:38 +0900

    Add files via upload
    using https://github.com/milesburton/Arduino-Temperature-Control-Library
    and https://github.com/PaulStoffregen/OneWire

---
+++ TeamHNZ_tempSsensor.ino
@@ -0,0 +1,20 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 10
+#define SENSOR_BIT 12
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  sensors.setResolution(SENSOR_BIT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensors.requestTemperatures();
+  Serial.println(sensors.getTempCByIndex(0));
+}
